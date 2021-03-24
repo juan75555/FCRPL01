c496d02331344b35cb63e45d9beac91ea882ad1e
juan75555 <72112435+juan75555@users.noreply.github.com>
2021-03-24 11:07:38 +0100

    Update Teamwork.cpp

--- Teamwork/Teamwork.cpp
+++ Teamwork/Teamwork.cpp
@@ -1,56 +1,7 @@
 #include <iostream>
-//#include <bitset>
 using namespace std;
 extern "C" bool IsValidAssembly(int a, int b, int c);
-char cadena[10];
-unsigned int num1;
-unsigned int num2;
 
-int main()
-{
-    cout << "Introduce la cadena: ";
-    cin >> cadena;
-    cout << cadena << endl;
-
-    cout << "Introduce el num1: ";
-    cin >> num1;
-    cout << num1 << endl;
-
-    cout << "Introduce el num2: ";
-    cin >> num2;
-    cout << num2 << endl;
-
-    //bitset<32> bs1(toBinary(num1));
-    //cout << "binary:  " << bs1 << endl;
-    //bitset<32> bs2(num1);
-    //cout << "binary:  " << bs2 << endl;
-    //
-    //bitset<32> bs3(toBinary(num2));
-    //cout << "binary:  " << bs3 << endl;
-    //bitset<32> bs4(num2);
-    //cout << "binary:  " << bs4 << endl;
-
-    //return EXIT_SUCCESS;
-    int decimal1;
-    short binario1[32];
-    decimal1 = num1;
-
-    for (int i = 0; i < 8; i++)
-    {
-        binario1[i] = decimal1 % 2;
-        decimal1 /= 2;
-
-    }
-
-    for (int i = 7; i >= 0; i--)
-    {
-        cout << binario1[i];
-    }
-    controlString(cadena);
-    cout << ControlWithMask(num1,num2) << endl;
-    cout << endl;
-    return 0;
-}
 void controlString(char* cadena) {
     if (strcmp(cadena, "2dBkF998Y") == 0) {
         cout << "Acceso Permitido" << endl;
@@ -59,7 +10,7 @@ void controlString(char* cadena) {
         cout << "Acceso Denegado" << endl;
         exit(1);
     }
-    if (strlen(cadena) < 10 && cadena[0] == cadena[9]) {
+    if (strlen(cadena) <= 10 && cadena[0] == cadena[9]) {
         cout << "Acceso Permitido" << endl;
     }
     else {
@@ -70,13 +21,6 @@ void controlString(char* cadena) {
 
 int ControlWithMask(unsigned int num1,unsigned int num2) 
 {
-    cout << "Introduce el num1: ";
-    cin >> num1;
-    cout << num1 << endl;
-
-    cout << "Introduce el num2: ";
-    cin >> num2;
-    cout << num2 << endl;
 
     int decimal1;
     int decimal2;
@@ -97,7 +41,6 @@ int ControlWithMask(unsigned int num1,unsigned int num2)
         decimal2 /= 2;
 
     }
-
     for (int i = 7; i >= 0; i--)
     {
         cout << binario1[i];
@@ -121,12 +64,48 @@ int ControlWithMask(unsigned int num1,unsigned int num2)
         exit(1);
     }
 }
-//string toBinary(int n)
-//{
-//    string r;
-//    while (n != 0) {
-//        r += (n % 2 == 0 ? "0" : "1");
-//        n /= 2;
-//    }
-//    return r;
-//}
+
+int CheckInlineAsmAccess() 
+{
+    __asm {
+
+  }
+}
+
+int main()
+{
+    char cadena[10];
+    unsigned int num1;
+    unsigned int num2;
+    int a;
+    int b;
+    int c;
+    cout << "Introduce la cadena: ";
+    cin >> cadena;
+    cout << cadena << endl;
+
+    cout << "Introduce el num1 (entero positivo): ";
+    cin >> num1;
+    cout << num1 << endl;
+
+    cout << "Introduce el num2 (entero positivo): ";
+    cin >> num2;
+    cout << num2 << endl;
+
+    cout << "Introduce el num3 (entero): ";
+    cin >> a;
+    cout << a << endl;
+
+    cout << "Introduce el num4 (entero): ";
+    cin >> b;
+    cout << b << endl;
+
+    cout << "Introduce el num5 (entero): ";
+    cin >> c;
+    cout << c << endl;
+
+    controlString(cadena);
+    ControlWithMask(num1, num2);
+    cout << endl;
+    return 0;
+}
