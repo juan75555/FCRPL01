bc870cdd84eec965095c86e406a4552c3258543e
juan75555 <72112435+juan75555@users.noreply.github.com>
2021-03-25 18:16:10 +0100

    Update Teamwork.cpp

--- Teamwork/Teamwork.cpp
+++ Teamwork/Teamwork.cpp
@@ -8,7 +8,7 @@ void controlString(char* cadena,char* cadena2) {
         cout << "Acceso Permitido" << endl;
     }
     else {
-        cout << "Acceso Denegado" << endl;
+       cout << "Acceso Denegado" << endl;
         exit(1);
     }
     if (strlen(cadena2) <= 10 && cadena2[0] == cadena2[9]) {
@@ -20,53 +20,190 @@ void controlString(char* cadena,char* cadena2) {
     }
 }
 
-int ControlWithMask(std::bitset<32> bs1, std::bitset<32> bs2)
+int ControlWithMask(std::bitset<32> bs1, std::bitset<32> bs2,int decimal)
 {
-   /* __asm{
-
-   }*/
-    /* if (bs1[26] == 0) {
-        cout << "Correct" << endl;
+     if (bs1[5] == 0) {
+        cout << "Correcto" << endl;
     }
     else {
         cout << "Hubo algún fallo" << endl;
         exit(1);
     }
-    if (bs1[19] == bs2[21]) {
-        cout << "Correct" << endl;
+    if (bs1[12] == bs2[10]) {
+        cout << "Correcto" << endl;
     }
     else {
         cout << "Hubo algún fallo" << endl;
         exit(1);
-    }*/
+    }
+    if (decimal == 3) {
+        cout << "Correcto" << endl;
+    }
+    else {
+        cout << "Hubo algún fallo" << endl;
+    }
     return 0;
 }
 
 int CheckInlineAsmAccess(std::bitset<32> bs6) 
 {
-    if (bs6[19] == bs6[18] && bs6[19] != bs6[13]) 
+    if (bs6[20] == bs6[19] && bs6[20] != bs6[14]) {
+    cout << "Entrada correcta" << endl;
+}
+else {
+    cout << "Entrada incorrecta" << endl;
+   exit(1);
+}
+   /* __asm 
     {
-        cout << "Entrada correcta" << endl;
-    }
-    else
-    {
-        cout << "Entrada incorrecta" << endl;
-        exit(1);
-    }
-    
-    
-    return 0;
+        push        13h
+        lea         eax, [ebp - 54h]
+        push        eax
+        lea         ecx, [bs6]
+        call        std::bitset<32>::operator[] (04026F0h)
+        mov         dword ptr[ebp - 1Ch], eax
+        mov         ecx, dword ptr[ebp - 1Ch]
+        mov         dword ptr[ebp - 20h], ecx
+        mov         dword ptr[ebp - 4], 0
+        mov         edx, dword ptr[ebp - 14h]
+        or edx, 1
+        mov         dword ptr[ebp - 14h], edx
+        mov         ecx, dword ptr[ebp - 20h]
+        call        std::bitset<32>::reference::operator bool(0402740h)
+        movzx       esi, al
+        push        12h
+        lea         eax, [ebp - 4Ch]
+        push        eax
+        lea         ecx, [bs6]
+        call        std::bitset<32>::operator[] (04026F0h)
+        mov         dword ptr[ebp - 24h], eax
+        mov         ecx, dword ptr[ebp - 24h]
+        mov         dword ptr[ebp - 28h], ecx
+        mov         dword ptr[ebp - 4], 1
+        mov         edx, dword ptr[ebp - 14h]
+        or edx, 2
+        mov         dword ptr[ebp - 14h], edx
+        mov         ecx, dword ptr[ebp - 28h]
+        call        std::bitset<32>::reference::operator bool(0402740h)
+        movzx       eax, al
+        cmp         esi, eax
+        jne         CheckInlineAsmAccess + 0F9h(04010F9h)
+        push        13h
+        lea         ecx, [ebp - 44h]
+        push        ecx
+        lea         ecx, [bs6]
+        call        std::bitset<32>::operator[] (04026F0h)
+        mov         dword ptr[ebp - 2Ch], eax
+        mov         edx, dword ptr[ebp - 2Ch]
+        mov         dword ptr[ebp - 30h], edx
+        mov         dword ptr[ebp - 4], 2
+        mov         eax, dword ptr[ebp - 14h]
+        or eax, 4
+        mov         dword ptr[ebp - 14h], eax
+        mov         ecx, dword ptr[ebp - 30h]
+        call        std::bitset<32>::reference::operator bool(0402740h)
+        movzx       esi, al
+        push        0Dh
+        lea         ecx, [ebp - 3Ch]
+        push        ecx
+        lea         ecx, [bs6]
+        call        std::bitset<32>::operator[] (04026F0h)
+        mov         dword ptr[ebp - 34h], eax
+        mov         edx, dword ptr[ebp - 14h]
+        or edx, 8
+        mov         dword ptr[ebp - 14h], edx
+        mov         ecx, dword ptr[ebp - 34h]
+        call        std::bitset<32>::reference::operator bool(0402740h)
+        movzx       eax, al
+        cmp         esi, eax
+        je          CheckInlineAsmAccess + 0F9h(04010F9h)
+        mov         dword ptr[ebp - 18h], 1
+        jmp         CheckInlineAsmAccess + 100h(0401100h)
+        mov         dword ptr[ebp - 18h], 0
+        mov         cl, byte ptr[ebp - 18h]
+        mov         byte ptr[ebp - 0Dh], cl
+        mov         edx, dword ptr[ebp - 14h]
+        and edx, 8
+        je          CheckInlineAsmAccess + 11Ah(040111Ah)
+        and dword ptr[ebp - 14h], 0FFFFFFF7h
+        lea         ecx, [ebp - 3Ch]
+        call        std::bitset<32>::reference::~reference(0402630h)
+        mov         dword ptr[ebp - 4], 1
+        mov         eax, dword ptr[ebp - 14h]
+        and eax, 4
+        je          CheckInlineAsmAccess + 135h(0401135h)
+        and dword ptr[ebp - 14h], 0FFFFFFFBh
+        lea         ecx, [ebp - 44h]
+        call        std::bitset<32>::reference::~reference(0402630h)
+        mov         dword ptr[ebp - 4], 0
+        mov         ecx, dword ptr[ebp - 14h]
+        and ecx, 2
+        je          CheckInlineAsmAccess + 150h(0401150h)
+        and dword ptr[ebp - 14h], 0FFFFFFFDh
+        lea         ecx, [ebp - 4Ch]
+        call        std::bitset<32>::reference::~reference(0402630h)
+        mov         dword ptr[ebp - 4], 0FFFFFFFFh
+        mov         edx, dword ptr[ebp - 14h]
+        and edx, 1
+        je          CheckInlineAsmAccess + 16Bh(040116Bh)
+        and dword ptr[ebp - 14h], 0FFFFFFFEh
+        lea         ecx, [ebp - 54h]
+        call        std::bitset<32>::reference::~reference(0402630h)
+        movzx       eax, byte ptr[ebp - 0Dh]
+        test        eax, eax
+        je          CheckInlineAsmAccess + 196h(0401196h)
+        push        offset std::endl<char, std::char_traits<char> >(0401EB0h)
+        push        4063C0h
+        mov         ecx, dword ptr[__imp_std::cout(0406060h)]
+        push        ecx
+        call        std::operator<<<std::char_traits<char> > (0401460h)
+        add         esp, 8
+        mov         ecx, eax
+        call        dword ptr[__imp_std::basic_ostream<char, std::char_traits<char> >::operator<< (0406048h)]
+        jmp         CheckInlineAsmAccess + 1BFh(04011BFh)
+        push        offset std::endl<char, std::char_traits<char> >(0401EB0h)
+        push        4063D4h
+        mov         edx, dword ptr[__imp_std::cout(0406060h)]
+        push        edx
+        call        std::operator<<<std::char_traits<char> > (0401460h)
+        add         esp, 8
+        mov         ecx, eax
+        call        dword ptr[__imp_std::basic_ostream<char, std::char_traits<char> >::operator<< (0406048h)]
+        push        1
+        call        dword ptr[__imp__exit(0406144h)]
+    }*/
+   return 0;
 }
 
-string toBinary(int n)
+string Decimal_Binario(int n)
 {
     string r;
     while (n != 0) {
         r += (n % 2 == 0 ? "0" : "1");
         n /= 2;
     }
+    string aux_cadena = "";
+
+    for (int i = r.size(); i >= 0; i--)
+        aux_cadena += r[i];
     return r;
 }
+
+int Binario_Decimal(int x,double decimal) {
+    int exp, digito;
+    exp = 0;
+    decimal = 0;
+    while (((int)(x / 10)) != 0)
+    {
+        digito = (int)x % 10;
+        decimal = decimal + digito * pow(2.0, exp);
+        exp++;
+        x = (int)(x / 10);
+    }
+    decimal = decimal + x * pow(2.0, exp);
+    return decimal;
+}
+
 
 int main()
 {
@@ -74,10 +211,12 @@ int main()
     char cadena2[10];
     unsigned int num1;
     unsigned int num2;
-    int num6;
-    //int a;
-    //int b;
-    //int c;
+    int num6;*/
+    int a;
+    int b;
+    int c;
+    double decimal;
+    
     cout << "Introduce una cadena: ";
     cin >> cadena;
     cout << cadena << endl;
@@ -94,7 +233,7 @@ int main()
     cin >> num2;
     cout << num2 << endl;
 
- /*   cout << "Introduce el num3 (entero): ";
+   cout << "Introduce el num3 (entero): ";
     cin >> a;
     cout << a << endl;
 
@@ -109,23 +248,15 @@ int main()
     cout << "introduce el num6 (entero): ";
     cin >> num6;
     cout << num6 << endl;
-  bitset<32> bs1(toBinary(num1));
-  bitset<32> bs2(toBinary(num2));
-  bitset<32> bs6(toBinary(num6));
-  
-
+  bitset<32> bs1(Decimal_Binario(num1));
+  bitset<32> bs2(Decimal_Binario(num2));
+  cout << bs2;
+  bitset<32> bs6(Decimal_Binario(num6));
+  int x = bs2[4]+bs2[5]+bs2[6];
+  Binario_Decimal(x,decimal);
     controlString(cadena,cadena2);
-    ControlWithMask(bs1,bs2);
+    ControlWithMask(bs1,bs2,decimal);
     CheckInlineAsmAccess(bs6);
     cout << endl;
     return 0;
 }
-//__asm {
-//        //    if (binary[19] == binary[18] && binary[19] /= binary[13]){
-//        //        cout << "Entrada correcta" << endl;
-//        //    }
-//        //    else {
-//        //        cout << "Entrada incorrecta" << endl;
-//        //        exit(1)
-//        //    }
-//        //}
